2630276a73944bbc9db3aa10baaed5df213ba50c
Taeho Lee <thlee@bizflow.com>
2017-02-10 22:03:09 -0500

    Jade to Pug

--- server.js
+++ server.js
@@ -3,75 +3,55 @@ var express = require('express'),
     logger = require('morgan'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose');
-var consts = require("./server.constants");
 
-var envMode = process.env.NODE_ENV = process.env.NODE_ENV || consts.ENVIRONMENT_MODE_DEVELOPMENT ;
-var port = process.env.PORT || 8000;
-//mongo ds147799.mlab.com:47799/taehovision -u <dbuser> -p <dbpassword>
-var dbUrl = envMode != consts.ENVIRONMENT_MODE_PRODUCTION ?
-            "mongodb://localhost:30001/taehovision"
-            :"mongodb://taeho:bizflow@ds147799.mlab.com:47799/taehovision";
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-// Jade Setting
-console.log("__dirname=" + __dirname);
-app.engine('jade', require('jade').__express);
-app.set('views', __dirname + '/server/views');
-app.set('view engine', 'jade');
-
-// Logger
-app.use(logger('dev'));
-
-
-// Variables
-var mongoMessage;
-
-// bodyParser
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-
-// Stylus
 function compile(str, path) {
     return stylus(str).set('filename', path);
 }
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'pug');
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.use(stylus.middleware(
     {
         src: __dirname + '/public',
         compile: compile
     }
 ));
-
-// Static middle ware
-//      public set up a static routing to the public directory below by using Express' static middleware.
 app.use(express.static(__dirname + '/public'));
 
-// MongoDB with Mongoose
-console.log(envMode + " dbUrl=" + dbUrl);
-mongoose.connect(dbUrl);
+if(env === 'development') {
+    mongoose.connect("mongodb://localhost:30001/taehovision");
+} else {
+    mongoose.connect("mongodb://taeho:bizflow@ds147799.mlab.com:47799/taehovision");
+}
 var db = mongoose.connection;
-db.on('error', ( function(err){
-    console.error(err.message);
-})).on('open', function() {
-    console.log('multivision db opened.');
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+    console.log('multivision db opened');
 });
-// create a schema - one field named message
-var messageSchema = mongoose.Schema({message:String});
-// create a model variable,
-var Message = mongoose.model('message', messageSchema);
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
 Message.findOne().exec(function(err, messageDoc) {
     mongoMessage = messageDoc.message;
 });
 
 app.get('/partials/:partialPath', function(req, res) {
-    console.log("get partials");
     res.render('partials/' + req.params.partialPath);
 });
 
 app.get('*', function(req, res) {
-    console.log("get *");
-    res.render('index', {mongoMessage:mongoMessage});
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
 });
 
+var port = process.env.PORT || 8000;
 app.listen(port);
 console.log('Listening on port ' + port + '...');
